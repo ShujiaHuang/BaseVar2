8956b466821cee8169ed07eb89be6956c2ca3930
Shujia Huang <huangshujia9@gmail.com>
2025-07-23 14:48:56 +0800

    commit version information

---
+++ src/version.h
@@ -0,0 +1,12 @@
+#ifndef BASEVAR_VERSION_H
+#define BASEVAR_VERSION_H
+
+#define BASEVAR_VERSION_MAJOR "2"
+#define BASEVAR_VERSION_MINOR "1"
+#define BASEVAR_VERSION_PATCH "0"
+#define BASEVAR_VERSION "2.1.0"
+#define BASEVAR_AUTHOR "Shujia Huang"
+#define BASEVAR_AUTHOR_EMAIL "hshujia@qq.com"
+#define BASEVAR_DESCRIPTION "BaseVar: Call variants from ultra low-pass (<1x) WGS data"
+
+#endif // BASEVAR_VERSION_H
