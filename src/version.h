36b8bcf67d308d8bcdbe0195b76baae7460bcebc
Shujia Huang <huangshujia9@gmail.com>
2025-07-24 17:00:39 +0800

    update the information of version

--- src/version.h
+++ src/version.h
@@ -7,6 +7,6 @@
 #define BASEVAR_VERSION "2.0.0"
 #define BASEVAR_AUTHOR "Shujia Huang"
 #define BASEVAR_AUTHOR_EMAIL "hshujia@qq.com"
-#define BASEVAR_DESCRIPTION "BaseVar: Call variants from ultra low-pass (<1x) WGS data"
+#define BASEVAR_DESCRIPTION "BaseVar: Call variants and estimate allele frequency from ultra low-pass (<1x) WGS data"
 
 #endif // BASEVAR_VERSION_H
