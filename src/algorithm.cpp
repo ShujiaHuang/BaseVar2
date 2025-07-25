38dcc4ab7628b4878e9493725425bd2e721e21e8
Shujia Huang <huangshujia9@gmail.com>
2025-07-25 12:12:05 +0800

    seperate the algorithm.h into two files

---
+++ src/algorithm.cpp
@@ -0,0 +1,213 @@
+#include "algorithm.h"
+
+// Function for chi^2 test
+double chi2_test(double chi_sqrt_value, double degree_of_freedom) {
+    return kf_gammaq(degree_of_freedom/2.0, chi_sqrt_value/2.0);
+}
+
+double norm_dist(double x) {
+    return kf_erfc(double(x / std::sqrt(2.0))) / 2.0;
+}
+
+double fisher_exact_test(int n11, int n12, int n21, int n22, TestSide test_side) {
+    // Input validation
+    if (n11 < 0 || n12 < 0 || n21 < 0 || n22 < 0) {
+        throw std::invalid_argument("Contingency table entries must be non-negative");
+    }
+
+    double left_pvalue, right_pvalue, twoside_pvalue;
+    kt_fisher_exact(n11, n12, n21, n22, 
+                    &left_pvalue, 
+                    &right_pvalue, 
+                    &twoside_pvalue);
+
+    double pvalue = -1.0;
+    switch(test_side) {
+        case TestSide::LESS:      pvalue = left_pvalue;    break;
+        case TestSide::GREATER:   pvalue = right_pvalue;   break;
+        case TestSide::TWO_SIDED: pvalue = twoside_pvalue; break;
+        default:
+            throw std::invalid_argument("Invalid test side specification");
+    }
+
+    return pvalue; // -1 is an error
+}
+
+double fisher_exact_test(const ContingencyTable& table, TestSide test_side) {
+    double left_pvalue, right_pvalue, twoside_pvalue;
+    kt_fisher_exact(table.n11, table.n12, table.n21, table.n22,
+                    &left_pvalue, &right_pvalue, &twoside_pvalue);
+    
+    double pvalue = -1.0;
+    switch(test_side) {
+        case TestSide::LESS:      pvalue = left_pvalue;    break;
+        case TestSide::GREATER:   pvalue = right_pvalue;   break;
+        case TestSide::TWO_SIDED: pvalue = twoside_pvalue; break;
+        default:
+            throw std::invalid_argument("Invalid test side specification");
+    }
+
+    return pvalue; // -1 is an error
+}
+
+double wilcoxon_ranksum_test(const std::vector<double>& sample1, const std::vector<double>& sample2) {
+
+    std::vector<double> combined = sample1;
+    combined.insert(combined.end(), sample2.begin(), sample2.end());
+
+    // 排序并分配秩
+    std::vector<size_t> rank_idx(combined.size());
+    std::iota(rank_idx.begin(), rank_idx.end(), 0.0); // 初始化秩
+    std::sort(rank_idx.begin(), rank_idx.end(), [&combined](size_t a, size_t b) { return combined[a] > combined[b]; });
+
+    std::vector<double> rankvalues(combined.size());
+    for (size_t i(0); i < rank_idx.size(); ++i) 
+        rankvalues[i] = i+1; 
+
+    // 处理重复元素
+    double ranksum = 0.0, same_n = 1;
+    size_t i;
+    for (i = 0; i < rank_idx.size(); ++i) {
+        if (i > 0 && combined[rank_idx[i]] != combined[rank_idx[i-1]]) {
+
+            if (same_n > 1) {
+                double avg_rank = ranksum / same_n; // 平均秩
+                for (size_t j = i - same_n; j < i; ++j) {
+                    rankvalues[j] = avg_rank; // 分配平均秩
+                }
+            }
+
+            // 重置
+            same_n  = 1;
+            ranksum = 0;
+        } else if (i > 0) {
+            same_n++;
+        }
+        ranksum += i+1;
+    }
+
+    // 处理最后一组重复
+    if (same_n > 1) {
+        double avg_rank = ranksum / same_n; // 平均秩
+        for (size_t j = i - same_n; j < i; ++j) {
+            rankvalues[j] = avg_rank; // 分配平均秩
+        }
+    }
+
+    size_t n1 = sample1.size(), n2 = sample2.size();
+
+    // 计算样本1的秩和
+    double smp1_ranksum = 0.0;
+    for (size_t i = 0; i < rank_idx.size(); ++i) {
+        if (rank_idx[i] < n1) {
+            smp1_ranksum += rankvalues[i];
+        }
+    }
+
+    double e = (double)(n1 * (n1 + n2 + 1)) / 2.0;
+    double z = (smp1_ranksum - e) / std::sqrt(double(n1*n2*(n1+n2+1))/12.0);
+    double p = 2 * norm_dist(std::abs(z));
+    
+    // 返回秩和检验 pvalue
+    return p;
+}
+
+void e_step(const std::vector<double> &obs_allele_freq,
+            const std::vector<std::vector<double>> &ind_allele_likelihood, 
+            std::vector<std::vector<double>> &ind_allele_post_prob,  // return value, update value inplace 
+            std::vector<double> &marginal_likelihood)                // return value, calculate value inplace
+{
+    size_t n_sample = ind_allele_likelihood.size();
+    size_t n_allele = obs_allele_freq.size();
+
+    // 'likelihood' is as the same shape as `ind_allele_likelihood`
+    std::vector<std::vector<double>> likelihood(n_sample, std::vector<double>(n_allele, 0));
+
+    // reset the raw value to be 0
+    marginal_likelihood = std::vector<double>(n_sample, 0);
+    for (size_t i(0); i < n_sample; ++i) {
+        for (size_t j = 0; j < n_allele; j++) {  // for _UNIQ_BASES
+            likelihood[i][j] = ind_allele_likelihood[i][j] * obs_allele_freq[j];
+            marginal_likelihood[i] += likelihood[i][j];
+        }
+
+        // Computed the posterior probability of _UNIQ_BASES, change the value inplace.
+        for (size_t j(0); j < n_allele; ++j) { 
+            // reset the posterior value
+            ind_allele_post_prob[i][j] = likelihood[i][j] / marginal_likelihood[i];
+        }
+    }
+
+    return;
+}
+
+void m_step(const std::vector<std::vector<double>> &ind_allele_post_prob, std::vector<double> &obs_allele_freq) {
+    size_t n_sample = ind_allele_post_prob.size();  // depth
+    size_t n_allele = ind_allele_post_prob[0].size();
+
+    // Reset data
+    obs_allele_freq = std::vector<double>(n_allele, 0);
+    for (size_t j(0); j < n_allele; ++j) {
+        for (size_t i(0); i < n_sample; ++i) {
+            obs_allele_freq[j] += ind_allele_post_prob[i][j];
+        }
+        obs_allele_freq[j] /= (double)(n_sample);  // average.
+    }
+
+    return;
+}
+
+void EM(const std::vector<std::vector<double>> &ind_allele_likelihood, // n x _UNIQ_BASES.size() matrix, do not change the raw value.
+        std::vector<double> &obs_allele_freq,          // retuen value, 1 x _UNIQ_BASES.size(), expect allele frequence, it'll be update inplace here.
+        std::vector<double> &log_marginal_likelihood,  // return value
+        int iter_num, const float epsilon)
+{
+    if (iter_num <= 0) {
+        throw std::invalid_argument("Iteration number must be positive");
+    }
+    if (epsilon <= 0) {
+        throw std::invalid_argument("Epsilon must be positive");
+    }
+
+    size_t n_sample = ind_allele_likelihood.size();
+    size_t n_allele = obs_allele_freq.size();
+
+    // n x _UNIQ_BASES.size()matrix, the same shape as 'ind_allele_likelihood'.
+    std::vector<std::vector<double>> ind_allele_post_prob = std::vector<std::vector<double>>(
+        n_sample, std::vector<double>(n_allele, 0));
+
+    // It's a 1-d array (n x 1) one sample per value, n is sample size
+    std::vector<double> marginal_likelihood = std::vector<double>(n_sample, 0);
+
+    // Update the value of 'ind_allele_post_prob' and compute 'marginal_likelihood' in e_step.
+    e_step(obs_allele_freq, ind_allele_likelihood, ind_allele_post_prob, marginal_likelihood);
+
+    log_marginal_likelihood = std::vector<double>(n_sample, 0);
+    for (size_t i = 0; i < marginal_likelihood.size(); i++) {
+        log_marginal_likelihood[i] = log(marginal_likelihood[i]);
+    }
+    
+    // use 'ind_allele_post_prob' to update 'obs_allele_freq'
+    m_step(ind_allele_post_prob, obs_allele_freq);
+    while (iter_num--) {
+        // e step: update ind_allele_post_prob
+        e_step(obs_allele_freq, ind_allele_likelihood, ind_allele_post_prob, marginal_likelihood);
+
+        // m step: update the frequence of observed alleles
+        m_step(ind_allele_post_prob, obs_allele_freq); 
+
+        double delta = 0, llh;
+        for (size_t i = 0; i < marginal_likelihood.size(); i++) {
+            llh = log(marginal_likelihood[i]);
+            delta += abs(llh - log_marginal_likelihood[i]);
+            log_marginal_likelihood[i] = llh;  // update
+        }
+        
+        // Todo: be careful here!!!
+        if (delta < epsilon) break;
+    }
+    // update the lastest expect_allele_freq
+    m_step(ind_allele_post_prob, obs_allele_freq);
+    
+    return;
+}
--- src/algorithm.h
+++ src/algorithm.h
@@ -32,108 +32,104 @@ inline size_t argmax(ForwardIterator first, ForwardIterator last) {
 }
 
 // sum the value for all the data which could call '+' operator
-template<typename T>
-T sum(const std::vector<T> &value) {
-    T d(0);
-    for (auto x: value) { d += x; }
-    
-    return d;
+template<typename T> // T must be a numeric type
+typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+sum(const std::vector<T> &values) {
+    if (values.empty()) {
+        return T(0);
+    }
+    return std::accumulate(values.begin(), values.end(), T(0));
+}
+
+// mean the value for all the data which could call '+' operator
+template<typename T>  // T must be a numeric type
+double mean(const std::vector<T> &values) {
+    if (values.empty()) {
+        throw std::invalid_argument("Cannot calculate mean of empty vector");
+    }
+    return static_cast<double>(sum(values)) / values.size();
+}
+
+// standard deviation the value for all the data which could call '+' operator
+template<typename T>  // T must be a numeric type
+double stddev(const std::vector<T> &values) {
+    if (values.size() < 2) {
+        throw std::invalid_argument("Cannot calculate standard deviation of less than 2 values");
+    }
+    double mean_value = mean(values);
+    double sum_squared_diff = 0.0;
+    for (const auto& value : values) {
+        sum_squared_diff += (value - mean_value) * (value - mean_value);
+    }
+    return std::sqrt(sum_squared_diff / (values.size() - 1));
+}
+
+// calculate the standard error of the mean
+template<typename T>  // T must be a numeric type
+double standard_error(const std::vector<T> &values) {
+    if (values.size() < 2) {
+        throw std::invalid_argument("Cannot calculate standard error of less than 2 values");
+    }
+    return stddev(values) / std::sqrt(values.size());
+}
+
+// median the value for all the data which could call '+' operator
+template<typename T>  // T must be a numeric type
+double median(std::vector<T> &value) {
+    size_t n = value.size();
+    if (n == 0) return 0.0;
+
+    std::sort(value.begin(), value.end());
+    if (n % 2 == 0) {
+        return (value[n/2-1] + value[n/2]) / 2.0;
+    } else {
+        return value[n/2];
+    }
 }
 
 // Function for chi^2 test
-double chi2_test(double chi_sqrt_value, double degree_of_freedom) {
-    return kf_gammaq(degree_of_freedom/2.0, chi_sqrt_value/2.0);
-}
+double chi2_test(double chi_sqrt_value, double degree_of_freedom);
+double norm_dist(double x);
 
-double norm_dist(double x) {
-    return kf_erfc(double(x / std::sqrt(2.0))) / 2.0;
-}
-
+enum class TestSide {
+    LESS,
+    GREATER,
+    TWO_SIDED
+};
 /**
  *  Perform a Fisher exact test on a 2x2 contingency table. 
  *  The null hypothesis is that the true odds ratio of the 
  *  populations underlying the observations is one.
+ *  
+ *  Mathmatical note: https://mathworld.wolfram.com/FishersExactTest.html
+ * 
+ *  @param n11 Value in cell (1,1)
+ *  @param n12 Value in cell (1,2)
+ *  @param n21 Value in cell (2,1)
+ *  @param n22 Value in cell (2,2)
+ *  @param test_side Type of test to perform (left-sided, right-sided, or two-sided)
+ *  @return p-value for the specified test
  * 
  *    n11  n12  | n1_
  *    n21  n22  | n2_
  *   -----------+----
  *    n_1  n_2  | n
+ * 
+ *  Example: https://gatk.broadinstitute.org/hc/en-us/articles/360035532152-Fisher-s-Exact-Test
+ * 
  */
-double fisher_exact_test(int n11, int n12, int n21, int n22, 
-                         bool is_leftside  = false,
-                         bool is_rightside = false,
-                         bool is_twoside   = true)
-{
-    double left_pvalue, right_pvalue, twoside_pvalue;
-    double v = kt_fisher_exact(n11, n12, n21, n22, 
-                               &left_pvalue, 
-                               &right_pvalue, 
-                               &twoside_pvalue);
-
-    return (is_twoside) ? twoside_pvalue : (is_leftside ? left_pvalue : right_pvalue);
-}
-
-double wilcoxon_ranksum_test(const std::vector<double>& sample1, const std::vector<double>& sample2) {
-
-    size_t n1 = sample1.size(), n2 = sample2.size();
-
-    std::vector<double> combined = sample1;
-    combined.insert(combined.end(), sample2.begin(), sample2.end());
-
-    // 排序并分配秩
-    std::vector<size_t> rank_idx(combined.size());
-    std::iota(rank_idx.begin(), rank_idx.end(), 0.0); // 初始化秩
-    std::sort(rank_idx.begin(), rank_idx.end(), [&combined](size_t a, size_t b) { return combined[a] > combined[b]; });
-
-    std::vector<double> rankvalues(combined.size());
-    for (size_t i(0); i < rank_idx.size(); ++i) 
-        rankvalues[i] = i+1; 
-
-    // 处理重复元素
-    double ranksum = 0.0, same_n = 1;
-    size_t i;
-    for (i = 0; i < rank_idx.size(); ++i) {
-        if (i > 0 && combined[rank_idx[i]] != combined[rank_idx[i-1]]) {
-
-            if (same_n > 1) {
-                double avg_rank = ranksum / same_n; // 平均秩
-                for (size_t j = i - same_n; j < i; ++j) {
-                    rankvalues[j] = avg_rank; // 分配平均秩
-                }
-            }
-
-            // 重置
-            same_n  = 1;
-            ranksum = 0;
-        } else if (i > 0) {
-            same_n++;
-        }
-        ranksum += i+1;
-    }
-
-    // 处理最后一组重复
-    if (same_n > 1) {
-        double avg_rank = ranksum / same_n; // 平均秩
-        for (size_t j = i - same_n; j < i; ++j) {
-            rankvalues[j] = avg_rank; // 分配平均秩
+double fisher_exact_test(int n11, int n12, int n21, int n22, TestSide test_side=TestSide::TWO_SIDED);
+struct ContingencyTable {
+    int n11, n12, n21, n22;
+    ContingencyTable(int a, int b, int c, int d) : n11(a), n12(b), n21(c), n22(d) {
+        if (n11 < 0 || n12 < 0 || n21 < 0 || n22 < 0) {
+            throw std::invalid_argument("Contingency table entries must be non-negative");
         }
     }
+};
+double fisher_exact_test(const ContingencyTable& table, TestSide test_side=TestSide::TWO_SIDED);
 
-    // 计算样本1的秩和
-    double smp1_ranksum = 0.0;
-    for (size_t i = 0; i < rank_idx.size(); ++i) {
-        if (rank_idx[i] < n1) {
-            smp1_ranksum += rankvalues[i];
-        }
-    }
-
-    double e = (double)(n1 * (n1 + n2 + 1)) / 2.0;
-    double z = (smp1_ranksum - e) / std::sqrt(double(n1*n2*(n1+n2+1))/12.0);
-    double p = 2 * norm_dist(std::abs(z));
-    
-    // 返回秩和检验 pvalue
-    return p;
-}
+double wilcoxon_ranksum_test(const std::vector<double>& sample1, const std::vector<double>& sample2);
 
 /**
  * @brief Calculate the posterior probability of individual allele at each site as
@@ -148,31 +144,7 @@ double wilcoxon_ranksum_test(const std::vector<double>& sample1, const std::vect
 void e_step(const std::vector<double> &obs_allele_freq,
             const std::vector<std::vector<double>> &ind_allele_likelihood, 
             std::vector<std::vector<double>> &ind_allele_post_prob,  // return value, update value inplace 
-            std::vector<double> &marginal_likelihood)                // return value, calculate value inplace
-{
-    size_t n_sample = ind_allele_likelihood.size();
-    size_t n_allele = obs_allele_freq.size();
-
-    // 'likelihood' is as the same shape as `ind_allele_likelihood`
-    std::vector<std::vector<double>> likelihood(n_sample, std::vector<double>(n_allele, 0));
-
-    // reset the raw value to be 0
-    marginal_likelihood = std::vector<double>(n_sample, 0);
-    for (size_t i(0); i < n_sample; ++i) {
-        for (size_t j = 0; j < n_allele; j++) {  // for [A, C, G, T]
-            likelihood[i][j] = ind_allele_likelihood[i][j] * obs_allele_freq[j];
-            marginal_likelihood[i] += likelihood[i][j];
-        }
-
-        // Computed the posterior probability of A/C/G/T for each individual, change the value inplace.
-        for (size_t j(0); j < n_allele; ++j) { 
-            // reset the posterior value
-            ind_allele_post_prob[i][j] = likelihood[i][j] / marginal_likelihood[i];
-        }
-    }
-
-    return;
-}
+            std::vector<double> &marginal_likelihood);               // return value, calculate value inplace
 
 /**
  * @brief Update observed allele frequency inplace.
@@ -181,21 +153,7 @@ void e_step(const std::vector<double> &obs_allele_freq,
  * @param obs_allele_freq      1d-array, 1 x 4. update the allele frequence inplace and return. 
  * 
  */
-void m_step(const std::vector<std::vector<double>> &ind_allele_post_prob, std::vector<double> &obs_allele_freq) {
-    size_t n_sample = ind_allele_post_prob.size();
-    size_t n_allele = ind_allele_post_prob[0].size();
-
-    // Reset data
-    obs_allele_freq = std::vector<double>(n_allele, 0);
-    for (size_t j(0); j < n_allele; ++j) {
-        for (size_t i(0); i < n_sample; ++i) {
-            obs_allele_freq[j] += ind_allele_post_prob[i][j];
-        }
-        obs_allele_freq[j] /= (double)(n_sample);  // average.
-    }
-
-    return;
-}
+void m_step(const std::vector<std::vector<double>> &ind_allele_post_prob, std::vector<double> &obs_allele_freq); 
 
 /**
  * @brief EM algorithm
@@ -210,48 +168,6 @@ void m_step(const std::vector<std::vector<double>> &ind_allele_post_prob, std::v
 void EM(const std::vector<std::vector<double>> &ind_allele_likelihood, // n x 4 matrix, do not change the raw value.
         std::vector<double> &obs_allele_freq,          // retuen value, 1 x 4, expect allele frequence, it'll be update inplace here.
         std::vector<double> &log_marginal_likelihood,  // return value
-        int iter_num=100, const float epsilon=0.001)
-{
-    size_t n_sample = ind_allele_likelihood.size();
-    size_t n_allele = obs_allele_freq.size();
-
-    // n x 4 matrix, the same shape as 'ind_allele_likelihood'.
-    std::vector<std::vector<double>> ind_allele_post_prob = std::vector<std::vector<double>>(
-        n_sample, std::vector<double>(n_allele, 0));
-
-    // It's a 1-d array (n x 1) one sample per value, n is sample size
-    std::vector<double> marginal_likelihood = std::vector<double>(n_sample, 0);
-
-    // Update the value of 'ind_allele_post_prob' and compute 'marginal_likelihood' in e_step.
-    e_step(obs_allele_freq, ind_allele_likelihood, ind_allele_post_prob, marginal_likelihood);
-
-    log_marginal_likelihood = std::vector<double>(n_sample, 0);
-    for (size_t i = 0; i < marginal_likelihood.size(); i++) {
-        log_marginal_likelihood[i] = log(marginal_likelihood[i]);
-    }
-    
-    // use 'ind_allele_post_prob' to update 'obs_allele_freq'
-    m_step(ind_allele_post_prob, obs_allele_freq);
-    while (iter_num--) {
-        // e step: update ind_allele_post_prob
-        e_step(obs_allele_freq, ind_allele_likelihood, ind_allele_post_prob, marginal_likelihood);
-
-        // m step: update the frequence of observed alleles
-        m_step(ind_allele_post_prob, obs_allele_freq); 
-
-        double delta = 0, llh;
-        for (size_t i = 0; i < marginal_likelihood.size(); i++) {
-            llh = log(marginal_likelihood[i]);
-            delta += abs(llh - log_marginal_likelihood[i]);
-            log_marginal_likelihood[i] = llh;  // update
-        }
-        
-        // Todo: be careful here!!!
-        if (delta < epsilon) break;
-    }
-    // update the lastest expect_allele_freq
-    m_step(ind_allele_post_prob, obs_allele_freq);
-    return;
-}
+        int iter_num=100, const float epsilon=0.001);
 
 #endif
