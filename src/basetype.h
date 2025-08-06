1fc228c3addd1d50050861f76a60806a11516b58
Shujia Huang <huangshujia9@gmail.com>
2025-08-06 09:35:36 +0800

    bug fix and reconstruct the codes

--- src/basetype.h
+++ src/basetype.h
@@ -3,7 +3,7 @@
  * @brief 
  * 
  * @author Shujia Huang
- * @date 2018-08-01
+ * @date 2025-03-01
  * 
  */
 #ifndef __INCLUDE_BASETYPE_H__
@@ -16,117 +16,107 @@
 #include <vector>
 #include <map>
 
-static const std::vector<char> BASES = {'A', 'C', 'G', 'T'}; // 定义这个值，限定 likelihood 数组中碱基似然值的存放顺序
-static const double MLN10TO10   = -0.23025850929940458;      // ln(10)/10，把 phred-value 换成 e 为底，方便调用 exp()
-static const int LRT_THRESHOLD  = 24;                        // 24 corresponding to a chi-pvalue of 10^-6
-static const int QUAL_THRESHOLD = 20;                        // -10 * lg(10^-2)
+#include "algorithm.h"
+#include "io/utils.h"              // join()
 
-// Mainly use for basevar variant callling
-struct BatchInfo {
+#include "external/combinations.h"
 
-    size_t n;
-
-    std::string ref_id;
-    std::string ref_base;
-    uint32_t    ref_pos;
-    uint32_t    depth; // The coverage depth on ref_pos
-
-    std::vector<std::string> align_bases;
-    std::vector<char>        align_base_quals;
-
-    std::vector<int>  mapqs;
-    std::vector<char> map_strands;
-    std::vector<int>  base_pos_ranks;
-
-    // Set default argument
-    BatchInfo(): n(0), ref_pos(0), depth(0) {}
-};
-
-/**
- * @brief Define a structure for recording allele information return by _f() 
- * in this class.
- * 
- */
-typedef struct {
-    std::vector<std::vector<char>> bc;
-    std::vector<std::vector<double>> bp;
-    std::vector<double> lr;
-} AA;
-
-typedef struct {
-    int ref_fwd, ref_rev;
-    int alt_fwd, alt_rev;
-    double fs;
-    double sor;
-} StrandBiasInfo;
+static const std::vector<std::string> BASIC_BASES = {"A", "C", "G", "T"}; // 预定义这个值，限定 UNIQ_BASES 数组中至少有这四个碱基
+static const int LRT_THRESHOLD  = 24;             // 24 corresponding to a chi-pvalue of 10^-6
+static const int QUAL_THRESHOLD = 20;             // -10 * lg(10^-2)
+static const double MLN10TO10   = -0.2302585093;  // -ln(10)/10 = -0.23025850929940458，换底，把 phred-value 换成 e 为底，方便调用 exp()
 
 // A class for calculate the base probability
 class BaseType {
 
 private:
-    bool _only_call_snp;
+    /**
+     * @brief Define a structure for recording allele information return by BaseType::_f() 
+     * in this class.
+     * 
+     */
+    struct AA {
+        std::vector<std::vector<std::string>> bc;
+        std::vector<std::vector<double>> bp;
+        std::vector<double> lr;
+    };
+
+    std::vector<std::string> _UNIQ_BASES;
+    std::map<std::string, size_t> _B_IDX;  // A map for recroding the base in _UNIQ_BASES => index
 
     std::string _ref_id;
-    std::string _ref_base;
     uint32_t _ref_pos;
-    std::vector<char> _alt_bases; // only call SNP for now   
+    std::vector<std::string> _active_bases;           // the Ref and alternative bases
+    std::map<std::string, std::string> _bases2ref;  // align/alt bases => ref_base
     double _var_qual;
     double _min_af;
 
-    // Estimated SNP allele frequency by EM and LRT
-    std::map<char, double> _af_by_lrt;
+    int _total_depth;                      // depth on ref_pos
+    std::map<std::string, double> _depth;  // bases depth, double 是为了方便做除法
 
-    // [A, C, G, T] likelihood vector for echo individual
-    std::vector<std::vector<double>> _ind_allele_likelihood; // 2d-array, n x 4 matrix, n is sample size.
-    std::vector<double> _qual_pvalue; // n x 1 matrix, n is sample size.
-    std::map<char, size_t> _B_IDX;    // A map for recrod the BASE => index
+    // Estimated base frequency of _UNIQ_BASES by LRT
+    std::map<std::string, double> _af_by_lrt;
+
+    // _UNIQ_BASES likelihood vector for echo base string
+    std::vector<std::vector<double>> _allele_likelihood; // 2d-array, n x _UNIQ_BASES.size() matrix, n is total_depth.
     
-    int _total_depth;               // sum depth of ACGT
-    std::map<char, double> _depth;  // allele depth of [A, C, G, T], double 是为了方便做除法
-
     // init the base likelihood by input bases
-    std::vector<double> _set_allele_initial_freq(const std::vector<char> &bases);
+    std::vector<double> _set_initial_freq(const std::vector<std::string> &bases);
 
     /**
      * @brief Calculate population likelihood for all the combination of bases
      * 
-     * @param bases A 1-d array. An array subset of bases from [A, C, G, T] 
+     * @param bases A 1-d array. An array subset of bases from _UNIQ_BASES 
      * @param n     The combination number. n must less or equal to the length of ``bases``
      * 
      * @return AA   AA.bc: An array of combinamtion bases
      *              AA.lr: Likelihood of ``bc``
      * 
      */
-    AA _f(const std::vector<char> &bases, int n);
+    AA _f(const std::vector<std::string> &bases, int n);
 
 public:
+    struct BatchInfo {
+        std::string ref_id;
+        uint32_t    ref_pos;
+
+        std::vector<std::string> ref_bases;
+        std::vector<std::string> align_bases;
+        std::vector<char>        align_base_quals;
+        std::vector<int>         base_pos_ranks;
+
+        std::vector<int>  mapqs;
+        std::vector<char> map_strands;
+
+        BatchInfo() : ref_id(""), ref_pos(0) {};
+        BatchInfo(const std::string& rid, uint32_t pos) : ref_id(rid), ref_pos(pos) {};
+    };
+
     // Constructor
     BaseType(){};
     BaseType(const BatchInfo *smp_bi, double af);
-    ~BaseType(){};
-
     BaseType(const BaseType &b);  // copy constructor
 
-    // The main function for likelihood ratio test
+    // destructor
+    ~BaseType(){};
+
     /**
-     * @brief 
+     * @brief The main function for likelihood ratio test
      * 
      * @param specific_bases a 1d-array. [optional]
      * Calculating LRT for specific base combination if provided.
      */
-    void lrt() { /* default */ lrt(BASES); }
-    void lrt(const std::vector<char> &specific_bases);
+    void lrt() { /* default */ lrt(_UNIQ_BASES); }
+    void lrt(const std::vector<std::string> &specific_bases);
 
-    const bool is_only_snp() const { return this->_only_call_snp; }
     const std::string &get_ref_id() const { return this->_ref_id; };
     const uint32_t &get_ref_pos() const { return this->_ref_pos; };
-    const std::string &get_ref_base() const { return this->_ref_base; };
-    const std::vector<char> &get_alt_bases() const { return this->_alt_bases; };
-    const std::vector<double> &get_qual_pvalue() const { return this->_qual_pvalue; }
+    const std::map<std::string, std::string> &get_bases2ref() const { return this->_bases2ref; };
+    const std::vector<std::string> &get_active_bases() const { return this->_active_bases; }; // candidate variant bases
 
     const double get_var_qual() const { return this->_var_qual; }
     const int get_total_depth() const { return this->_total_depth; }
-    const double get_base_depth(char b) const {
+    const double get_base_depth(const std::string &b) const {
         // operator[] doesn't have a 'const' qualifier in std::map. Use 'at' instead in C++11
         // https://stackoverflow.com/questions/42095642/error-passing-const-stdmapint-int-as-this-argument-discards-qualifiers
         double d;
@@ -136,10 +126,10 @@ public:
             std::string what_ex(ex.what());
             throw std::runtime_error("[ERROR] out_of_range:: " + what_ex + " '"+ b + "' not found.");
         }
-        return d; 
+        return d;
     }
 
-    const double get_lrt_af(char b) const { 
+    const double get_lrt_af(const std::string &b) const { 
         double lrt_af;
         try {
             lrt_af = this->_af_by_lrt.at(b);
@@ -149,35 +139,7 @@ public:
         }
         return lrt_af; 
     }
-
+    
 }; // BaseType class
-
-/**
- * @brief Mann-Whitney-Wilcoxon Rank Sum Test for REF and ALT array.
- * 
- * @param ref_base A reference base
- * @param alt_bases_string 
- * @param bases 
- * @param values 
- * @return double   Phred-scale value of ranksum-test pvalue
- * 
- * Note: There's some difference between scipy.stats.ranksums with R's wilcox.test:
- *       https://stackoverflow.com/questions/12797658/pythons-scipy-stats-ranksums-vs-rs-wilcox-test
- * 
- */
-double ref_vs_alt_ranksumtest(const char ref_base, 
-                              const std::string alt_bases_string,
-                              const std::vector<char> &bases,
-                              const std::vector<int> &values);
-
-double ref_vs_alt_ranksumtest(const char ref_base, 
-                              const std::string alt_bases_string,
-                              const std::vector<char> &bases,
-                              const std::vector<char> &values);
-
-StrandBiasInfo strand_bias(const char ref_base, 
-                           const std::string alt_bases_string,
-                           const std::vector<char> &bases,
-                           const std::vector<char> &strands);
 
 #endif
