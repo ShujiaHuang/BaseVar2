425b19abe7e200d1b96e1ff051c31065aee14e86
Shujia Huang <huangshujia9@gmail.com>
2026-05-20 21:03:09 +0800

    delete bin/manual_install.sh

--- bin/manual_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# In MacOS
-g++ -O3 -fPIC ../src/*.cpp ../src/io/*.cpp ../htslib/libhts.a -I ../htslib -lz -lbz2 -lm -llzma -lpthread -lcurl -o basevar
-
-# In Linux
-g++ -O3 -fPIC ../src/*.cpp ../src/io/*.cpp ../htslib/libhts.a -I ../htslib -lz -lbz2 -lm -llzma -lpthread -lcurl -lssl -lcrypto -o basevar
-
-
